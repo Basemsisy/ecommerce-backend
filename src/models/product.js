1d54a757c67a3968c10afd43897a53a68cefad79
BasemElsisy <basemsisy@gmail.com>
2021-01-04 12:17:00 +0200

    feat: products schema

--- src/models/product.js
+++ src/models/product.js
@@ -3,9 +3,51 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
-  count: {
+  description: {
     type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [{ type: String }],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
