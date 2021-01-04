a46575b44e57a940fe2a523e95314f9b637d809c
BasemElsisy <basemsisy@gmail.com>
2021-01-04 12:19:25 +0200

    feat: category schema

--- src/models/category.js
+++ src/models/category.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+});
 
 exports.Category = mongoose.model("Category", categorySchema);
