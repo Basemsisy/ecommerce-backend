3ac704908028cf990c8e0a08beda5e86899895ca
BasemElsisy <basemsisy@gmail.com>
2021-01-04 12:00:11 +0200

    change folder structure

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv/config");
+
+const api = process.env.API_PREFIX;
+const CategoriesRouter = require("./routers/category");
+const ordersRouter = require("./routers/order");
+const productsRouter = require("./routers/product");
+const usersRouter = require("./routers/user");
 
 // middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
-require("dotenv/config");
+//Routers
+app.use(`${api}/categories`, CategoriesRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/users`, usersRouter);
 
 mongoose
   .connect(process.env.DB_URL, {
