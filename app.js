79aeb139e15dba3708f41551be537669840b5528
BasemElsisy <basemsisy@gmail.com>
2021-01-04 08:41:20 +0200

    add .env variables, install body parser

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+// middlewares
+app.use(bodyParser.json());
+
+require("dotenv/config");
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`server runing on port: ${PORT}`);
 });
