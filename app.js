82666dccc36079252b53765ba0d2b8a83a79409b
BasemElsisy <basemsisy@gmail.com>
2021-01-04 09:20:16 +0200

    setup mongoose, connect to database

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // middlewares
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 require("dotenv/config");
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "ecommerce-db",
+  })
+  .then(() => {
+    console.log("database connection is ready");
+  })
+  .catch((err) => {
+    console.log("faild to connect to db", err);
+  });
 
 const PORT = process.env.PORT || 5000;
 
