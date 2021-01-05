420258cfb4cc75ae348b0c52309a87a89e6c930d
BasemElsisy <basemsisy@gmail.com>
2021-01-05 09:44:47 +0200

    feat: add product

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
 
 const { Product } = require("../models/product");
+const { Category } = require("../models/category");
 
-router.post(`/`, (req, res) => {
-  const newProduct = new Product(req.body);
-  newProduct
-    .save()
-    .then((product) => {
-      res.send(product);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+router.post(`/`, async (req, res) => {
+  try {
+    const cat = await Category.findById(req.body.category);
+    if (cat) {
+      const newProduct = new Product(req.body);
+      newProduct
+        .save()
+        .then((product) => {
+          res.send(product);
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+    } else res.status(404).json({ message: "not found this category" });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
