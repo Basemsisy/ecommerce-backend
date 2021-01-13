98c5e29159b8d452939a2a7f3310af2da01e42b5
BasemElsisy <basemsisy@gmail.com>
2021-01-13 10:11:50 +0200

    feat: users  routes

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,13 +1,58 @@
 const router = require("express").Router();
 const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/", async (req, res) => {
   try {
-    const usersList = await User.find();
+    const usersList = await User.find().select("-hashPassword");
     res.send(usersList);
   } catch (err) {
     res.send(err);
   }
 });
 
+router.post("/", (req, res) => {
+  const user = new User(req.body);
+  user.hashPassword = bcrypt.hashSync(req.body.password, 10);
+  user
+    .save()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .select("-hashPassword")
+    .then((user) => {
+      if (user) res.send(user);
+      else res.send("not found this user");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user && bcrypt.compareSync(req.body.password, user.hashPassword)) {
+      const secret = process.env.SECRET;
+      const token = jwt.sign(
+        {
+          id: user.id,
+        },
+        secret,
+        { expiresIn: "1d" }
+      );
+      res.send({ ...user._doc, token });
+    } else res.send("not found this user");
+  } catch (error) {
+    res.send(error);
+  }
+});
 module.exports = router;
