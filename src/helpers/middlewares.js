163f9bd83dca26dd5f00cd9f1c2067d7771251d7
BasemElsisy <basemsisy@gmail.com>
2021-01-13 11:41:00 +0200

    feat: exclude some routes from authMiddleware

--- src/helpers/middlewares.js
+++ src/helpers/middlewares.js
@@ -2,7 +2,14 @@ const expressJwt = require("express-jwt");
 
 function authMiddleware() {
   const secret = process.env.SECRET;
-  return expressJwt({ secret, algorithms: ["HS256"] });
+  return expressJwt({ secret, algorithms: ["HS256"] }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, method: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, method: ["GET", "OPTIONS"] },
+      "/api/v1/users/login ",
+      "/api/v1/users/register ",
+    ],
+  });
 }
 
 module.exports = authMiddleware;
